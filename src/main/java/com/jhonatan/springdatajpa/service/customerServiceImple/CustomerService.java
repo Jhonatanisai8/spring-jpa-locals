634be379c83a2c6d3880fcdd66cc985777ff7914
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-02-10 10:17:33 -0500

    interface que definira los metodos a implementar

---
+++ src/main/java/com/jhonatan/springdatajpa/service/customerServiceImple/CustomerService.java
@@ -0,0 +1,7 @@
+package com.jhonatan.springdatajpa.service.customerServiceImple;
+
+import com.jhonatan.springdatajpa.models.Customer;
+
+public interface CustomerService {
+    Customer saveCustomer(Customer customer);
+}
